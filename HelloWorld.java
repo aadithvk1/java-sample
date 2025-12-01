1bfac1e7c46f3885db7166c51f7a5f3ccad45b44
Aadith VK <aadithvk01@gmail.com>
2025-12-01 11:37:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!! Good Morning.");
 		System.out.println("Today is a beautiful day!");
 	}
 }
