ae7fe1b88f307845384c61e40ee4358fd7ea7380
Aadith VK <aadithvk01@gmail.com>
2025-12-04 08:49:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("A.P.J. Abdul Kalam was the 11th President of India. He served as president from 2002 to 2007! ");
+		System.out.println("A.P.J. Abdul Kalam was the 11th President of India. He served as president from 2002 to 2007!");
 	}
 }
